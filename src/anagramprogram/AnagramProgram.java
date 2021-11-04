880c40f0e5005ae4d61554f2126e1f74a950e9a8
Alejandro Jaimes <juanalejo2010@gmail.com>
2021-11-04 13:41:22 -0500

    Initialice repositorie

---
+++ src/anagramprogram/AnagramProgram.java
@@ -0,0 +1,12 @@
+
+package anagramprogram;
+/**
+ *
+ * @author Alejandro Jaimes
+ */
+public class AnagramProgram {
+    public static void main(String[] args) {
+        System.out.println("Anagrams");
+    }
+    
+}
