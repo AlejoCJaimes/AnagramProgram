c4926984c7ded9767eea3b02f84894cf364db670
Alejandro Jaimes <juanalejo2010@gmail.com>
2021-11-04 14:34:14 -0500

    Version2

--- src/anagramprogram/AnagramProgram.java
+++ src/anagramprogram/AnagramProgram.java
@@ -7,6 +7,7 @@ package anagramprogram;
 public class AnagramProgram {
     public static void main(String[] args) {
         System.out.println("Anagrams");
+        System.out.println("Version2");
     }
     
 }
